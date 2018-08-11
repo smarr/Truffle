971624308652ccf94c60f9a084b83e5686e2e9c6
Stefan Marr <git@stefan-marr.de>
2018-08-11 22:18:13 +0100

    [HACK] When instrumenting nodes, ignore ProbeNode and its children
    - this should be dropped
    - a proper solution should take the .isInstrumentable from the parent
      and decide whether we are in a ProbeNode and whether instrumentation
      is fine or not.
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- truffle/src/com.oracle.truffle.api.instrumentation/src/com/oracle/truffle/api/instrumentation/InstrumentationHandler.java
+++ truffle/src/com.oracle.truffle.api.instrumentation/src/com/oracle/truffle/api/instrumentation/InstrumentationHandler.java
@@ -1764,6 +1764,11 @@ final class InstrumentationHandler {
 
         @Override
         public boolean visit(Node originalNode) {
+            // SM: Ignore the ProbeNode and its children
+            if (originalNode instanceof ProbeNode) {
+                return false;
+            }
+
             Node node = originalNode;
             SourceSection sourceSection = node.getSourceSection();
             boolean instrumentable = InstrumentationHandler.isInstrumentableNode(node);
