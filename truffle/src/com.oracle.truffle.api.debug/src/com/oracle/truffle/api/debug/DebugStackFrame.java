e9bd618af9deef322bd49989e98d1b45ab1b9deb
Stefan Marr <git@stefan-marr.de>
2020-06-10 22:52:09 +0100

    Added @since annotations
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- truffle/src/com.oracle.truffle.api.debug/src/com/oracle/truffle/api/debug/DebugStackFrame.java
+++ truffle/src/com.oracle.truffle.api.debug/src/com/oracle/truffle/api/debug/DebugStackFrame.java
@@ -412,6 +412,9 @@ public final class DebugStackFrame {
         return findTruffleFrame(FrameAccess.READ_ONLY);
     }
 
+    /**
+     * @since smarr/debugger
+     */
     public RootNode getRootNode() {
         return findCurrentRoot();
     }
