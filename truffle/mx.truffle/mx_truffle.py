aaf678de7694392d33d3058ffdefda3fcff3dbf7
Christian Humer <christian.humer@oracle.com>
2023-08-23 15:26:56 +0200

    Fix TRUFFLE_JSON was missing in the graalvm component.

--- truffle/mx.truffle/mx_truffle.py
+++ truffle/mx.truffle/mx_truffle.py
@@ -1699,6 +1699,7 @@ mx_sdk_vm.register_graalvm_component(mx_sdk_vm.GraalVmTruffleLibrary(
         'sdk:JNIUTILS',
         'truffle:TRUFFLE_API',
         'truffle:TRUFFLE_RUNTIME',
+        'truffle:TRUFFLE_JSON',
     ],
     stability="supported",
 ))
