86380af443036dde6497f61d83c7f522dd52f1fb
Stefan Marr <git@stefan-marr.de>
2024-04-23 15:47:18 +0100

    Remove isComputeBytecode detection, and use isMyBytecodeLoop
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/nodes/StructuredGraph.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/nodes/StructuredGraph.java
@@ -411,8 +411,6 @@ public final class StructuredGraph extends Graph implements JavaMethodContext {
     private OptimizationLog optimizationLog;
 
     public final boolean isMyBytecodeLoop;
-    public final boolean isComputeBytecode;
-// public final boolean isBytecodeLoop;
 
     private StructuredGraph(String name,
                     ResolvedJavaMethod method,
@@ -447,15 +445,6 @@ public final class StructuredGraph extends Graph implements JavaMethodContext {
         this.cacheInvalidationListener = new CacheInvalidationListener();
 
         isMyBytecodeLoop = isMyBytecodeLoop(method);
-        isComputeBytecode = isComputeBytecode(method);
-    }
-
-    private static boolean isComputeBytecode(ResolvedJavaMethod method) {
-        if (method == null) {
-            return false;
-        }
-        String className = method.getDeclaringClass().getName();
-        return className.contains("ComputeBytecode") && method.getName().startsWith("executeGeneric");
     }
 
     private static boolean isMyBytecodeLoop(ResolvedJavaMethod method) {
