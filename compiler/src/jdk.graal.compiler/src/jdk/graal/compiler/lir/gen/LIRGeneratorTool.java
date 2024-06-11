a12fb5e96460283e0829010b576bca5ac146c8d9
Stefan Marr <git@stefan-marr.de>
2024-06-11 14:07:47 +0100

    Implement markBlockAsBytecodeHandlerStart also for AMD64
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/lir/gen/LIRGeneratorTool.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/lir/gen/LIRGeneratorTool.java
@@ -576,7 +576,7 @@ public interface LIRGeneratorTool extends CoreProviders, DiagnosticLIRGeneratorT
      */
     void emitSpeculationFence();
 
-    default void markBlockAsBytecodeHandlerStart(int bytecodeHandlerIndex) {}
+    void markBlockAsBytecodeHandlerStart(int bytecodeHandlerIndex);
 
     /**
      * Write value to the protection key register.
