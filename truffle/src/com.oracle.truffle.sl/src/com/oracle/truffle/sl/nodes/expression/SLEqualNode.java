winbinz/graal

Initial implementation of the OperationDSL by Nikola Bebic.

truffle/src/com.oracle.truffle.sl/src/com/oracle/truffle/sl/nodes/expression/SLEqualNode.java
@@ -68,40 +68,40 @@ import com.oracle.truffle.sl.runtime.SLNull;
 public abstract class SLEqualNode extends SLBinaryNode {
 
     @Specialization
-    protected boolean doLong(long left, long right) {
+    public static boolean doLong(long left, long right) {
         return left == right;
     }
 
     @Specialization
     @TruffleBoundary
-    protected boolean doBigNumber(SLBigNumber left, SLBigNumber right) {
+    public static boolean doBigNumber(SLBigNumber left, SLBigNumber right) {
         return left.equals(right);
     }
 
     @Specialization
-    protected boolean doBoolean(boolean left, boolean right) {
+    public static boolean doBoolean(boolean left, boolean right) {
         return left == right;
     }
 
     @Specialization
-    protected boolean doString(String left, String right) {
+    public static boolean doString(String left, String right) {
         return left.equals(right);
     }
 
     @Specialization
-    protected boolean doTruffleString(TruffleString left, TruffleString right,
+    public static boolean doTruffleString(TruffleString left, TruffleString right,
                     @Cached TruffleString.EqualNode equalNode) {
         return equalNode.execute(left, right, SLLanguage.STRING_ENCODING);
     }
 
     @Specialization
-    protected boolean doNull(SLNull left, SLNull right) {
+    public static boolean doNull(SLNull left, SLNull right) {
         /* There is only the singleton instance of SLNull, so we do not need equals(). */
         return left == right;
     }
 
     @Specialization
-    protected boolean doFunction(SLFunction left, Object right) {
+    public static boolean doFunction(SLFunction left, Object right) {
         /*
          * Our function registry maintains one canonical SLFunction object per function name, so we
          * do not need equals().
@@ -124,7 +124,7 @@ public abstract class SLEqualNode extends SLBinaryNode {
      * replace the previous specializations, as they are still more efficient in the interpeter.
      */
     @Specialization(limit = "4")
-    public boolean doGeneric(Object left, Object right,
+    public static boolean doGeneric(Object left, Object right,
                     @CachedLibrary("left") InteropLibrary leftInterop,
                     @CachedLibrary("right") InteropLibrary rightInterop) {
         /*

truffle/src/com.oracle.truffle.sl/src/com/oracle/truffle/sl/nodes/expression/SLLessThanNode.java
@@ -41,8 +41,10 @@
 package com.oracle.truffle.sl.nodes.expression;
 
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
+import com.oracle.truffle.api.dsl.Bind;
 import com.oracle.truffle.api.dsl.Fallback;
 import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.nodes.NodeInfo;
 import com.oracle.truffle.sl.SLException;
 import com.oracle.truffle.sl.nodes.SLBinaryNode;
@@ -56,19 +58,19 @@ import com.oracle.truffle.sl.runtime.SLBigNumber;
 public abstract class SLLessThanNode extends SLBinaryNode {
 
     @Specialization
-    protected boolean lessThan(long left, long right) {
+    public static boolean lessThan(long left, long right) {
         return left < right;
     }
 
     @Specialization
     @TruffleBoundary
-    protected boolean lessThan(SLBigNumber left, SLBigNumber right) {
+    public static boolean lessThan(SLBigNumber left, SLBigNumber right) {
         return left.compareTo(right) < 0;
     }
 
     @Fallback
-    protected Object typeError(Object left, Object right) {
-        throw SLException.typeError(this, left, right);
+    public static Object typeError(Object left, Object right, @Bind("$root") Node node, @Bind("$bci") int bci) {
+        throw SLException.typeError(node, "<", bci, left, right);
     }
 
 }

truffle/src/com.oracle.truffle.sl/src/com/oracle/truffle/sl/nodes/expression/SLSubNode.java
@@ -41,8 +41,10 @@
 package com.oracle.truffle.sl.nodes.expression;
 
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
+import com.oracle.truffle.api.dsl.Bind;
 import com.oracle.truffle.api.dsl.Fallback;
 import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.nodes.NodeInfo;
 import com.oracle.truffle.sl.SLException;
 import com.oracle.truffle.sl.nodes.SLBinaryNode;
@@ -55,19 +57,19 @@ import com.oracle.truffle.sl.runtime.SLBigNumber;
 public abstract class SLSubNode extends SLBinaryNode {
 
     @Specialization(rewriteOn = ArithmeticException.class)
-    protected long sub(long left, long right) {
+    public static long subLong(long left, long right) {
         return Math.subtractExact(left, right);
     }
 
-    @Specialization
+    @Specialization(replaces = "subLong")
     @TruffleBoundary
-    protected SLBigNumber sub(SLBigNumber left, SLBigNumber right) {
+    public static SLBigNumber sub(SLBigNumber left, SLBigNumber right) {
         return new SLBigNumber(left.getValue().subtract(right.getValue()));
     }
 
     @Fallback
-    protected Object typeError(Object left, Object right) {
-        throw SLException.typeError(this, left, right);
+    public static Object typeError(Object left, Object right, @Bind("$root") Node node, @Bind("$bci") int bci) {
+        throw SLException.typeError(node, "-", bci, left, right);
     }
 
 }

truffle/src/com.oracle.truffle.sl/src/com/oracle/truffle/sl/operations/SLOperationSerialization.java
@@ -1,0 +1,154 @@
+/*
+ * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * The Universal Permissive License (UPL), Version 1.0
+ *
+ * Subject to the condition set forth below, permission is hereby granted to any
+ * person obtaining a copy of this software, associated documentation and/or
+ * data (collectively the "Software"), free of charge and under any and all
+ * copyright rights in the Software, and any and all patent rights owned or
+ * freely licensable by each licensor hereunder covering either (i) the
+ * unmodified Software as contributed to or provided by such licensor, or (ii)
+ * the Larger Works (as defined below), to deal in both
+ *
+ * (a) the Software, and
+ *
+ * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+ * one is included with the Software each a "Larger Work" to which the Software
+ * is contributed by such licensors),
+ *
+ * without restriction, including without limitation the rights to copy, create
+ * derivative works of, display, perform, and distribute the Software and make,
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
+ *
+ * This license is subject to the following condition:
+ *
+ * The above copyright notice and either this complete permission notice or at a
+ * minimum a reference to the UPL must be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.oracle.truffle.sl.operations;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.util.function.Supplier;
+
+import com.oracle.truffle.api.operation.OperationConfig;
+import com.oracle.truffle.api.operation.OperationNodes;
+import com.oracle.truffle.api.operation.OperationParser;
+import com.oracle.truffle.api.operation.serialization.SerializationUtils;
+import com.oracle.truffle.api.source.Source;
+import com.oracle.truffle.api.strings.TruffleString;
+import com.oracle.truffle.sl.SLLanguage;
+import com.oracle.truffle.sl.runtime.SLBigNumber;
+import com.oracle.truffle.sl.runtime.SLNull;
+
+public final class SLOperationSerialization {
+
+    private static final byte CODE_SL_NULL = 0;
+    private static final byte CODE_STRING = 1;
+    private static final byte CODE_LONG = 2;
+    private static final byte CODE_SOURCE = 3;
+    private static final byte CODE_BIG_INT = 5;
+    private static final byte CODE_BOOLEAN_TRUE = 6;
+    private static final byte CODE_BOOLEAN_FALSE = 7;
+
+    private SLOperationSerialization() {
+        // no instances
+    }
+
+    public static byte[] serializeNodes(OperationParser<SLOperationRootNodeGen.Builder> parser) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        DataOutputStream outputStream = new DataOutputStream(byteArrayOutputStream);
+
+        SLOperationRootNodeGen.serialize(OperationConfig.COMPLETE, outputStream, (context, buffer, object) -> {
+            if (object instanceof SLNull) {
+                buffer.writeByte(CODE_SL_NULL);
+            } else if (object instanceof TruffleString) {
+                TruffleString str = (TruffleString) object;
+                buffer.writeByte(CODE_STRING);
+                writeString(buffer, str);
+            } else if (object instanceof Long) {
+                buffer.writeByte(CODE_LONG);
+                buffer.writeLong((long) object);
+            } else if (object instanceof Boolean) {
+                buffer.writeByte(((boolean) object) ? CODE_BOOLEAN_TRUE : CODE_BOOLEAN_FALSE);
+            } else if (object instanceof SLBigNumber) {
+                SLBigNumber num = (SLBigNumber) object;
+                buffer.writeByte(CODE_BIG_INT);
+                writeByteArray(buffer, num.getValue().toByteArray());
+            } else if (object instanceof Source) {
+                Source s = (Source) object;
+                buffer.writeByte(CODE_SOURCE);
+                writeByteArray(buffer, s.getName().getBytes());
+            } else {
+                throw new UnsupportedOperationException("unsupported constant: " + object.getClass().getSimpleName() + " " + object);
+            }
+        }, parser);
+
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    static void writeString(DataOutput buffer, TruffleString str) throws IOException {
+        writeByteArray(buffer, str.getInternalByteArrayUncached(SLLanguage.STRING_ENCODING).getArray());
+    }
+
+    private static byte[] readByteArray(DataInput buffer) throws IOException {
+        int len = buffer.readInt();
+        byte[] dest = new byte[len];
+        buffer.readFully(dest);
+        return dest;
+    }
+
+    private static void writeByteArray(DataOutput buffer, byte[] data) throws IOException {
+        buffer.writeInt(data.length);
+        buffer.write(data);
+    }
+
+    public static OperationNodes<SLOperationRootNode> deserializeNodes(SLLanguage language, byte[] inputData) throws IOException {
+        Supplier<DataInput> input = () -> SerializationUtils.createDataInput(ByteBuffer.wrap(inputData));
+        return SLOperationRootNodeGen.deserialize(language, OperationConfig.DEFAULT, input, (context, buffer) -> {
+            byte tag;
+            switch (tag = buffer.readByte()) {
+                case CODE_SL_NULL:
+                    return SLNull.SINGLETON;
+                case CODE_STRING:
+                    return readString(buffer);
+                case CODE_LONG:
+                    return buffer.readLong();
+                case CODE_BOOLEAN_TRUE:
+                    return Boolean.TRUE;
+                case CODE_BOOLEAN_FALSE:
+                    return Boolean.FALSE;
+                case CODE_BIG_INT:
+                    return new SLBigNumber(new BigInteger(readByteArray(buffer)));
+                case CODE_SOURCE: {
+                    String name = new String(readByteArray(buffer));
+                    return Source.newBuilder(SLLanguage.ID, "", name).build();
+                }
+                default:
+                    throw new UnsupportedOperationException("unsupported tag: " + tag);
+            }
+        });
+    }
+
+    static TruffleString readString(DataInput buffer) throws IOException {
+        return TruffleString.fromByteArrayUncached(readByteArray(buffer), SLLanguage.STRING_ENCODING);
+    }
+}

smarr/Truffle

truffle/src/com.oracle.truffle.sl/src/com/oracle/truffle/sl/nodes/expression/SLEqualNode.java
@@ -68,40 +68,40 @@ import com.oracle.truffle.sl.runtime.SLNull;
 public abstract class SLEqualNode extends SLBinaryNode {
 
     @Specialization
-    protected boolean doLong(long left, long right) {
+    public static boolean doLong(long left, long right) {
         return left == right;
     }
 
     @Specialization
     @TruffleBoundary
-    protected boolean doBigNumber(SLBigInteger left, SLBigInteger right) {
+    public static boolean doBigNumber(SLBigInteger left, SLBigInteger right) {
         return left.equals(right);
     }
 
     @Specialization
-    protected boolean doBoolean(boolean left, boolean right) {
+    public static boolean doBoolean(boolean left, boolean right) {
         return left == right;
     }
 
     @Specialization
-    protected boolean doString(String left, String right) {
+    public static boolean doString(String left, String right) {
         return left.equals(right);
     }
 
     @Specialization
-    protected boolean doTruffleString(TruffleString left, TruffleString right,
+    public static boolean doTruffleString(TruffleString left, TruffleString right,
                     @Cached TruffleString.EqualNode equalNode) {
         return equalNode.execute(left, right, SLLanguage.STRING_ENCODING);
     }
 
     @Specialization
-    protected boolean doNull(SLNull left, SLNull right) {
+    public static boolean doNull(SLNull left, SLNull right) {
         /* There is only the singleton instance of SLNull, so we do not need equals(). */
         return left == right;
     }
 
     @Specialization
-    protected boolean doFunction(SLFunction left, Object right) {
+    public static boolean doFunction(SLFunction left, Object right) {
         /*
          * Our function registry maintains one canonical SLFunction object per function name, so we
          * do not need equals().
@@ -124,7 +124,7 @@ public abstract class SLEqualNode extends SLBinaryNode {
      * replace the previous specializations, as they are still more efficient in the interpeter.
      */
     @Specialization(limit = "4")
-    public boolean doGeneric(Object left, Object right,
+    public static boolean doGeneric(Object left, Object right,
                     @CachedLibrary("left") InteropLibrary leftInterop,
                     @CachedLibrary("right") InteropLibrary rightInterop) {
         /*

truffle/src/com.oracle.truffle.sl/src/com/oracle/truffle/sl/nodes/expression/SLLessThanNode.java
@@ -43,11 +43,13 @@ package com.oracle.truffle.sl.nodes.expression;
 import static com.oracle.truffle.api.CompilerDirectives.shouldNotReachHere;
 
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
+import com.oracle.truffle.api.dsl.Bind;
 import com.oracle.truffle.api.dsl.Fallback;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.interop.InteropLibrary;
 import com.oracle.truffle.api.interop.UnsupportedMessageException;
 import com.oracle.truffle.api.library.CachedLibrary;
+import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.nodes.NodeInfo;
 import com.oracle.truffle.sl.SLException;
 import com.oracle.truffle.sl.nodes.SLBinaryNode;
@@ -61,19 +63,19 @@ import com.oracle.truffle.sl.runtime.SLBigInteger;
 public abstract class SLLessThanNode extends SLBinaryNode {
 
     @Specialization
-    protected boolean doLong(long left, long right) {
+    public static boolean doLong(long left, long right) {
         return left < right;
     }
 
     @Specialization
     @TruffleBoundary
-    protected boolean doSLBigInteger(SLBigInteger left, SLBigInteger right) {
+    public static boolean doSLBigInteger(SLBigInteger left, SLBigInteger right) {
         return left.compareTo(right) < 0;
     }
 
     @Specialization(replaces = "doSLBigInteger", guards = {"leftLibrary.fitsInBigInteger(left)", "rightLibrary.fitsInBigInteger(right)"}, limit = "3")
     @TruffleBoundary
-    protected boolean doInteropBigInteger(Object left, Object right,
+    public static boolean doInteropBigInteger(Object left, Object right,
                     @CachedLibrary("left") InteropLibrary leftLibrary,
                     @CachedLibrary("right") InteropLibrary rightLibrary) {
         try {
@@ -84,8 +86,8 @@ public abstract class SLLessThanNode extends SLBinaryNode {
     }
 
     @Fallback
-    protected Object typeError(Object left, Object right) {
-        throw SLException.typeError(this, left, right);
+    public static Object typeError(Object left, Object right, @Bind("$root") Node node, @Bind("$bci") int bci) {
+        throw SLException.typeError(node, "<", bci, left, right);
     }
 
 }

truffle/src/com.oracle.truffle.sl/src/com/oracle/truffle/sl/nodes/expression/SLSubNode.java
@@ -43,11 +43,13 @@ package com.oracle.truffle.sl.nodes.expression;
 import static com.oracle.truffle.api.CompilerDirectives.shouldNotReachHere;
 
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
+import com.oracle.truffle.api.dsl.Bind;
 import com.oracle.truffle.api.dsl.Fallback;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.interop.InteropLibrary;
 import com.oracle.truffle.api.interop.UnsupportedMessageException;
 import com.oracle.truffle.api.library.CachedLibrary;
+import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.nodes.NodeInfo;
 import com.oracle.truffle.sl.SLException;
 import com.oracle.truffle.sl.nodes.SLBinaryNode;
@@ -60,19 +62,19 @@ import com.oracle.truffle.sl.runtime.SLBigInteger;
 public abstract class SLSubNode extends SLBinaryNode {
 
     @Specialization(rewriteOn = ArithmeticException.class)
-    protected long doLong(long left, long right) {
+    public static long doLong(long left, long right) {
         return Math.subtractExact(left, right);
     }
 
-    @Specialization
+    @Specialization(replaces = "doLong")
     @TruffleBoundary
-    protected SLBigInteger doSLBigInteger(SLBigInteger left, SLBigInteger right) {
+    public static SLBigInteger doSLBigInteger(SLBigInteger left, SLBigInteger right) {
         return new SLBigInteger(left.getValue().subtract(right.getValue()));
     }
 
     @Specialization(replaces = "doSLBigInteger", guards = {"leftLibrary.fitsInBigInteger(left)", "rightLibrary.fitsInBigInteger(right)"}, limit = "3")
     @TruffleBoundary
-    protected SLBigInteger doInteropBigInteger(Object left, Object right,
+    public static SLBigInteger doInteropBigInteger(Object left, Object right,
                     @CachedLibrary("left") InteropLibrary leftLibrary,
                     @CachedLibrary("right") InteropLibrary rightLibrary) {
         try {
@@ -83,8 +85,8 @@ public abstract class SLSubNode extends SLBinaryNode {
     }
 
     @Fallback
-    protected Object typeError(Object left, Object right) {
-        throw SLException.typeError(this, left, right);
+    public static Object typeError(Object left, Object right, @Bind("$root") Node node, @Bind("$bci") int bci) {
+        throw SLException.typeError(node, "-", bci, left, right);
     }
 
 }

truffle/src/com.oracle.truffle.sl/src/com/oracle/truffle/sl/operations/SLOperationSerialization.java
@@ -1,0 +1,154 @@
+/*
+ * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * The Universal Permissive License (UPL), Version 1.0
+ *
+ * Subject to the condition set forth below, permission is hereby granted to any
+ * person obtaining a copy of this software, associated documentation and/or
+ * data (collectively the "Software"), free of charge and under any and all
+ * copyright rights in the Software, and any and all patent rights owned or
+ * freely licensable by each licensor hereunder covering either (i) the
+ * unmodified Software as contributed to or provided by such licensor, or (ii)
+ * the Larger Works (as defined below), to deal in both
+ *
+ * (a) the Software, and
+ *
+ * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+ * one is included with the Software each a "Larger Work" to which the Software
+ * is contributed by such licensors),
+ *
+ * without restriction, including without limitation the rights to copy, create
+ * derivative works of, display, perform, and distribute the Software and make,
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
+ *
+ * This license is subject to the following condition:
+ *
+ * The above copyright notice and either this complete permission notice or at a
+ * minimum a reference to the UPL must be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.oracle.truffle.sl.operations;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.util.function.Supplier;
+
+import com.oracle.truffle.api.operation.OperationConfig;
+import com.oracle.truffle.api.operation.OperationNodes;
+import com.oracle.truffle.api.operation.OperationParser;
+import com.oracle.truffle.api.operation.serialization.SerializationUtils;
+import com.oracle.truffle.api.source.Source;
+import com.oracle.truffle.api.strings.TruffleString;
+import com.oracle.truffle.sl.SLLanguage;
+import com.oracle.truffle.sl.runtime.SLBigInteger;
+import com.oracle.truffle.sl.runtime.SLNull;
+
+public final class SLOperationSerialization {
+
+    private static final byte CODE_SL_NULL = 0;
+    private static final byte CODE_STRING = 1;
+    private static final byte CODE_LONG = 2;
+    private static final byte CODE_SOURCE = 3;
+    private static final byte CODE_BIG_INT = 5;
+    private static final byte CODE_BOOLEAN_TRUE = 6;
+    private static final byte CODE_BOOLEAN_FALSE = 7;
+
+    private SLOperationSerialization() {
+        // no instances
+    }
+
+    public static byte[] serializeNodes(OperationParser<SLOperationRootNodeGen.Builder> parser) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        DataOutputStream outputStream = new DataOutputStream(byteArrayOutputStream);
+
+        SLOperationRootNodeGen.serialize(OperationConfig.COMPLETE, outputStream, (context, buffer, object) -> {
+            if (object instanceof SLNull) {
+                buffer.writeByte(CODE_SL_NULL);
+            } else if (object instanceof TruffleString) {
+                TruffleString str = (TruffleString) object;
+                buffer.writeByte(CODE_STRING);
+                writeString(buffer, str);
+            } else if (object instanceof Long) {
+                buffer.writeByte(CODE_LONG);
+                buffer.writeLong((long) object);
+            } else if (object instanceof Boolean) {
+                buffer.writeByte(((boolean) object) ? CODE_BOOLEAN_TRUE : CODE_BOOLEAN_FALSE);
+            } else if (object instanceof SLBigInteger) {
+                SLBigInteger num = (SLBigInteger) object;
+                buffer.writeByte(CODE_BIG_INT);
+                writeByteArray(buffer, num.getValue().toByteArray());
+            } else if (object instanceof Source) {
+                Source s = (Source) object;
+                buffer.writeByte(CODE_SOURCE);
+                writeByteArray(buffer, s.getName().getBytes());
+            } else {
+                throw new UnsupportedOperationException("unsupported constant: " + object.getClass().getSimpleName() + " " + object);
+            }
+        }, parser);
+
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    static void writeString(DataOutput buffer, TruffleString str) throws IOException {
+        writeByteArray(buffer, str.getInternalByteArrayUncached(SLLanguage.STRING_ENCODING).getArray());
+    }
+
+    private static byte[] readByteArray(DataInput buffer) throws IOException {
+        int len = buffer.readInt();
+        byte[] dest = new byte[len];
+        buffer.readFully(dest);
+        return dest;
+    }
+
+    private static void writeByteArray(DataOutput buffer, byte[] data) throws IOException {
+        buffer.writeInt(data.length);
+        buffer.write(data);
+    }
+
+    public static OperationNodes<SLOperationRootNode> deserializeNodes(SLLanguage language, byte[] inputData) throws IOException {
+        Supplier<DataInput> input = () -> SerializationUtils.createDataInput(ByteBuffer.wrap(inputData));
+        return SLOperationRootNodeGen.deserialize(language, OperationConfig.DEFAULT, input, (context, buffer) -> {
+            byte tag;
+            switch (tag = buffer.readByte()) {
+                case CODE_SL_NULL:
+                    return SLNull.SINGLETON;
+                case CODE_STRING:
+                    return readString(buffer);
+                case CODE_LONG:
+                    return buffer.readLong();
+                case CODE_BOOLEAN_TRUE:
+                    return Boolean.TRUE;
+                case CODE_BOOLEAN_FALSE:
+                    return Boolean.FALSE;
+                case CODE_BIG_INT:
+                    return new SLBigInteger(new BigInteger(readByteArray(buffer)));
+                case CODE_SOURCE: {
+                    String name = new String(readByteArray(buffer));
+                    return Source.newBuilder(SLLanguage.ID, "", name).build();
+                }
+                default:
+                    throw new UnsupportedOperationException("unsupported tag: " + tag);
+            }
+        });
+    }
+
+    static TruffleString readString(DataInput buffer) throws IOException {
+        return TruffleString.fromByteArrayUncached(readByteArray(buffer), SLLanguage.STRING_ENCODING);
+    }
+}